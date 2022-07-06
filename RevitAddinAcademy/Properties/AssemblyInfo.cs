f29b6644048cdd1ab452e9861652cdb06a960b32
Cameron Flessner <cflessner@specializedeng.com>
2022-07-05 19:36:22 -0500

    Add project files.

---
+++ RevitAddinAcademy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RevitAddinAcademy")]
+[assembly: AssemblyDescription("Revit Add-In Description for RevitAddinAcademy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Specialized Engineering Solutions")]
+[assembly: AssemblyProduct("Revit Addin Academy Test File")]
+[assembly: AssemblyCopyright("Copyright (C) 2022 by Cameron Flessner")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60C6F352-39E3-4B35-B7CE-32DD999AFA06")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2022.0.0.0")]
+[assembly: AssemblyFileVersion("2022.0.0.0")]
